24ccc8e7eb1ec49ba6e4a97c11d700848f697928
Simone Andrea Muscas <simoneandrea.muscas@gmail.com>
2025-01-12 11:05:28 +0100

    Fix base rest exception
      LIB1

--- LibraryLoans.Core/BaseClasses/BaseRestException.cs
+++ LibraryLoans.Core/BaseClasses/BaseRestException.cs
@@ -7,7 +7,7 @@ public class BaseRestException : Exception
     public HttpStatusCode StatusCode { get; }
     public object Error { get; }
 
-    public BaseRestException(HttpStatusCode statusCode, string errorMessage)
+    public BaseRestException(HttpStatusCode statusCode, string errorMessage) : base(errorMessage)
     {
         StatusCode = statusCode;
         Error = new { error = errorMessage };
